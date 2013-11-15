a64f8cf81f80831e40abfad48153856fe004c004
MIke Sheridan <sherdog@gmail.com>
2013-11-14 20:46:29 -0600

    updated added jquery and added facebook connection with route

--- app.js
+++ app.js
@@ -16,4 +16,20 @@ app.get('/', function(req, res) {
     //res.send('Hello from <a href="http://appfog.com">AppFog.com</a>');
     res.render('index', {sample:'fakevalue'});
 });
+
+app.post('/friends', function(req, res){
+	var fbsdk = require('facebook-sdk');
+	var facebook_app_id = '1411854682381866';
+	var facebook_app_secret = '77c637ca1dcad889fbedd216ddbff4a7';
+
+	var facebook = new fbsdk.Facebook({
+	  appId  : facebook_app_id,
+	  secret : facebook_app_secret
+	});
+
+	facebook.api('/'+ facebook_app_id, function(data) {
+	  console.log(data);
+	});
+});
+
 app.listen(process.env.VCAP_APP_PORT || 3000);
