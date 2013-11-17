aad153d536961ece965ddc262a3e8357fc684f6c
MIke Sheridan <sherdog@gmail.com>
2013-11-17 09:17:56 -0600

    Added couchbase and facebook sdks

--- app.js
+++ app.js
@@ -17,19 +17,43 @@ app.get('/', function(req, res) {
     res.render('index', {sample:'fakevalue'});
 });
 
-app.post('/friends', function(req, res){
-	var fbsdk = require('facebook-sdk');
+app.get('/friends', function(req, res){
+
+	var fbsdk = require('facebook-node-sdk');
+
 	var facebook_app_id = '1411854682381866';
 	var facebook_app_secret = '77c637ca1dcad889fbedd216ddbff4a7';
 
-	var facebook = new fbsdk.Facebook({
-	  appId  : facebook_app_id,
-	  secret : facebook_app_secret
+	var Facebook = require('facebook-node-sdk');
+
+	var couchbase = require('couchbase');
+
+	var db = new couchbase.Connection({ bucket:'goldfish', host:'localhost:8091', password:'michael5'}, function(err) {
+	  if (err) {
+	    console.log('Connection Error', err);
+	  } else {
+	   db.get('player_11142090', function(err, result) {
+	   	if (err) throw err;
+	   		res.json(result);
+	      // Doc Value: Hello World!
+	    });
+	  }
 	});
 
+	function errorHandler(from, e) {
+
+	    console.log('Function: ' + from);
+	    console.log(e.message);
+	    console.log(e.stack);
+
+	}
+
+	/*
 	facebook.api('/'+ facebook_app_id, function(data) {
-	  console.log(data);
+	  res.json(data);
 	});
+	*/
+
 });
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
